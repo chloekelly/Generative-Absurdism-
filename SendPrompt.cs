aac6f15dedbe1b9dab1725792730e51326fb59f0
chloekelly <61448061+chloekelly@users.noreply.github.com>
2020-07-09 11:34:20 +1000

    A send script using OSC to send prompts to Runway
    This script was created by using and editing code from Thomas Fredericks Unity OSC repository. https://github.com/thomasfredericks/UnityOSC

---
+++ SendPrompt.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendPrompt : MonoBehaviour
+{
+    public OSC osc;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        osc.SetAddressHandler("/data", OnReceiveText);
+    }
+
+
+    void OnReceiveText(OscMessage message)
+    {
+        Debug.Log("received!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            OscMessage message;
+
+            message = new OscMessage();
+            message.address = "/server/connect";
+            osc.Send(message);
+
+
+            message = new OscMessage();
+            message.address = "/query";
+            message.values.Add("{\"prompt\":\"I have no pennies.\"}");
+            osc.Send(message);
+
+            Debug.Log("sent");
+
+            OscMessage recievee;
+
+            recievee = new OscMessage();
+            recievee.address = "/data";
+            recievee.("generated_text" );
+            osc.Send(message);
+
+            Debug.Log("generated_text");
+
+        }
+
+
+    }
+
+ 
+}
