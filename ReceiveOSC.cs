af93231249a9e2b6e44980d6e1e2b013bedaa9e0
chloekelly <61448061+chloekelly@users.noreply.github.com>
2020-08-12 21:22:18 +1000

    Update ReceiveOSC.cs
    Osc update to allow for String Split

--- ReceiveOSC.cs
+++ ReceiveOSC.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +8,9 @@ public class ReceiveOSC : MonoBehaviour
     public OSC osc;
     public TextToSpeech tts;
     public string transcript;
+    public string split;
+    string phrase;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +26,20 @@ public class ReceiveOSC : MonoBehaviour
     }
 
     void OnData(OscMessage message)
+
     {
+       
         string words = message.ToString();
+       
         words = words.Substring(words.IndexOf(transcript) + transcript.Length);
         words = words.Replace("\\n", "");
         words = words.Replace("\"", "");
         words = words.Replace("}", "");
         words = words.Replace("{", "");
+
+        string[] phrase = words.Split("."[0]);
+        words = phrase[0];
+
         words = words.Replace(".", " ");
         words = words.Replace("ESTRAGON", "    ");
         words = words.Replace("VLADIMIR", "    ");
@@ -38,6 +48,7 @@ public class ReceiveOSC : MonoBehaviour
         words = words.Replace("Pause", " ");
         words = words.Replace("Mr Paradock", "     ");
         words = words.Replace("Mrs Paradock", " ");
+ 
 
 
         tts.dialogue = words;
